13305e561eb9fedd3ac8615759cc5098345bbce8
Benhur <thebenhur.cr@gmail.com>
2024-04-02 05:20:57 -0300

    MegaSena

---
+++ app/src/main/java/com/benhu/megasena/MainActivity.kt
@@ -0,0 +1,74 @@
+package com.benhu.megasena
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.annotation.ReturnThis
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.benhu.megasena.ui.theme.MegaSenaTheme
+import java.util.Random
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val editText: EditText = findViewById(R.id.edit_number)
+        val txtResult: TextView = findViewById(R.id.txt_result)
+        val btnGenerate: Button = findViewById(R.id.bnt_generate)
+
+        btnGenerate.setOnClickListener {
+            val text = editText.text.toString()
+
+            numberGenerator(text, txtResult)
+        }
+    }
+
+    private fun numberGenerator(text: String, txtResult: TextView) {
+
+        if (text.isNotEmpty()) {
+
+            val qtd = text.toInt()
+
+            if (qtd >= 6 && qtd <= 15) {
+
+                val numbers = mutableSetOf<Int>()
+                val random = Random()
+
+                while (true) {
+                    val number = random.nextInt(60)
+                    numbers.add(number + 1)
+
+                    if (numbers.size == qtd) {
+                        break
+                    }
+                }
+                txtResult.text = numbers.sorted().joinToString(" - ")
+
+            } else {
+                Toast.makeText(this, "Infome um número entre 6 e 15!", Toast.LENGTH_SHORT).show()
+            }
+
+        } else {
+            Toast.makeText(this, "Informe um número entre 6 e 15!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+}
+
+
+
+
+
